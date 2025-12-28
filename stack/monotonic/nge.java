1ac1f17f5cd27f69adce0c0c1587ebc2fd331bec
Athisankar-2006 <athisankar2k6@gmail.com>
2025-12-28 22:27:34 +0530

    monotonic pattern

---
+++ stack/monotonic/nge.java
@@ -0,0 +1,14 @@
+package monotonic;
+
+import java.util.Stack;
+
+//next greatest element
+
+public class nge {
+    public static void main(String[] args) {
+        Stack<Integer> stack=new Stack<>();
+        stack.push(10);
+        stack.push(20);
+        System.out.println(stack);
+    }
+}
